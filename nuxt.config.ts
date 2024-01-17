7f3abe1f823e8a0a2c0c06c5fdfec598dff48750
Napha Moonngoen <napa_tom@hotmail.com>
2024-01-17 14:35:19 +0700

    fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
-    baseURL: process.env.NODE_ENV == "production" ? "/nuxt" : "/",
+    baseURL: process.env.NODE_ENV == "production" ? "/admin" : "/",
   },
 });
